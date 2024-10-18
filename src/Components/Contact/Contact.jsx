20d15eeb50eef347369149577daf911e012fd577
Aniket <kumaraniket900@gmail.com>
2024-10-18 17:58:37 +0530

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "./Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import contact_icon from "../../assets/contact-icon.png";
+import address_icon from "../../assets/address-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "ca1bd9fd-b110-4327-9bd3-2b9e396819a2");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} alt="msg_icon" />
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>
+            <img src={mail_icon} alt="mail_icon" />
+            Contact@Me.dev
+          </li>
+          <li>
+            <img src={contact_icon} alt="contact_icon" />
+            +1 123-246-7890
+          </li>
+          <li>
+            <img src={address_icon} alt="address_icon" />
+            77 Massachusetts Ave, Cambridge
+            <br />
+            MA 02139, United States
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Write your message here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit now
+            <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import "./VideoPlayer.css";
+import video from "../../assets/college-video.mp4";
+import { useRef } from "react";
+
+const VideoPlayer = ({ playState, setPlayState }) => {
+  const player = useRef(null);
+
+  const closePlayer = (e) => {
+    if (e.target === player.current) {
+      setPlayState(false);
+    }
+  };
+
+  return (
+    <div
+      className={`video-player ${playState ? "" : "hide"}`}
+      ref={player}
+      onClick={closePlayer}
+    >
+      <video src={video} autoPlay muted controls></video>
+    </div>
+  );
+};
+
+VideoPlayer.propTypes = {
+  playState: PropTypes.string.isRequired,
+  setPlayState: PropTypes.string.isRequired,
+};
+
+export default VideoPlayer;
